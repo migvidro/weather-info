ebd06ad848bec53558f3d5017610aba28d83079f
Miguel Vidigal <migvidro@gmail.com>
2025-01-05 19:35:18 -0300

    using units and language on api call

--- src/services/weather_service.ts
+++ src/services/weather_service.ts
@@ -5,7 +5,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const fetchWeather = async (city: string): Promise<WeatherResponse> => {
     const API_KEY = process.env.API_KEY;
-    const response = await axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}`);
+    const response = await axios.get(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`);
 
     return response.data;
 }
